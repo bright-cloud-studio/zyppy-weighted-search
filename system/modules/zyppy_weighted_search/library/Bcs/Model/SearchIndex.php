608697c735c93a542e51a74120f601e3d87e9160
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-05-18 13:31:35 -0400

    Create SearchIndex.php

---
+++ system/modules/zyppy_weighted_search/library/Bcs/Model/SearchIndex.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Zyppy Weighted Search
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/zyppy-weighted-search
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+namespace Bcs\Model;
+
+class SearchIndex extends \Model
+{
+	/* Table name */
+	protected static $strTable = 'tl_search';	
+}
