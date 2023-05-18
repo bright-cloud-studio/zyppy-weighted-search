f0296c271b38f34d5bd7caa5672a58ab3bffc520
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-05-18 13:27:02 -0400

    Create modules.php

---
+++ system/modules/zyppy_weighted_search/languages/en/modules.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Zyppy Weighted Search
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/zyppy-weighted-search
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* Back end modules */
+$GLOBALS['TL_LANG']['MOD']['search_index']          = array('Search Index', 'Displays entries in the auto-generated Search Index');
